0111c8fb25cd7e8e283488a9def55913a9d1a849
romanOVF <rkovriginpost@google.com>
2019-09-03 16:09:31 +0300

    some change

--- script.py
+++ script.py
@@ -12,37 +12,36 @@ def get_time():
 
 def get_news_echo_msk():
     
-    html = urlopen("https://echo.msk.ru/")
-    bs_obj = bs(html, 'lxml')
-    time_news = bs_obj.find('div', class_ = 'newspreview').find_all('span', class_ = 'datetime')
-    text_news = bs_obj.find('ul', class_ = 'newslist').find_all('h3')
+    html_echo = urlopen("https://echo.msk.ru/")
+    bs_obj = bs(html_echo, 'lxml')
+    text_news_echo = bs_obj.find('ul', class_ = 'newslist').find_all('h3')
     print('=== ECHO MOSCOW - NEWS ===', end = '')
     get_time()
 
-    for i in text_news:
+    for i in text_news_echo:
         print(" ".join(i.text.split()))
 
     print()
 
 def get_newsru():
-    html = urlopen("https://www.newsru.com/allnews/")
-    bs_obj = bs(html, 'lxml')
-    texts = bs_obj.find('body').find_all('a', class_ = 'index-news-title')
-    time_news = bs_obj.find('body').find_all('span', class_ = 'index-news-date')
+    html_newsru = urlopen("https://www.newsru.com/allnews/")
+    bs_obj = bs(html_newsru, 'lxml')
+    text_news_newsru = bs_obj.find('body').find_all('a', class_ = 'index-news-title')
+    time_news_newsru = bs_obj.find('body').find_all('span', class_ = 'index-news-date')
     print('=== NEWSRU.COM - NEWS ===', end = '')
     get_time()
 
-    time_list = []
-    news_list = []
+    time_list_newsru = []
+    news_list_newsru = []
 
-    for j in time_news:
-        time_list.append(j.text[13:-47])
+    for j in time_news_newsru:
+        time_list_newsru.append(j.text[13:-47])
 
-    for i in texts:
-        news_list.append(i.text[6:-5])
+    for i in text_news_newsru:
+        news_list_newsru.append(i.text[6:-5])
 
-    for k in range(len(time_list)):
-        all_string_news = "".join(time_list[k].split()) + ' ' + news_list[k]
+    for k in range(len(time_list_newsru)):
+        all_string_news = "".join(time_list_newsru[k].split()) + ' ' + news_list_newsru[k]
         print(all_string_news)
 
 get_news_echo_msk()
