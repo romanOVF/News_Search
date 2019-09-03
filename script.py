aebba8f98a410c2fa49a5fc548e6eca2134531e3
romanOVF <rkovriginpost@google.com>
2019-09-03 22:14:22 +0300

    added try and except

--- script.py
+++ script.py
@@ -3,46 +3,51 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
-import re, time
+import time
 
 def get_time():
     date_time = time.ctime(time.time())
     time_string = '{} {}'.format(' ', date_time)
     print(time_string)
 
-def get_news_echo_msk():
-    
-    html_echo = urlopen("https://echo.msk.ru/")
-    bs_obj = bs(html_echo, 'lxml')
-    text_news_echo = bs_obj.find('ul', class_ = 'newslist').find_all('h3')
-    print('=== ECHO MOSCOW - NEWS ===', end = '')
-    get_time()
+def get_news_echo_msk():    
+    try:
+        html_echo = urlopen("https://echo.msk.ru/")
+        bs_obj = bs(html_echo, 'lxml')
+        text_news_echo = bs_obj.find('ul', class_ = 'newslist').find_all('h3')
+        print('=== ECHO MOSCOW - NEWS ===', end = '')
+        get_time()
 
-    for i in text_news_echo:
-        print(" ".join(i.text.split()))
+        for i in text_news_echo:
+            print(" ".join(i.text.split()))
 
-    print()
+        print()
+    except Exception as exc:
+        print('Some problem ... echo.msk.ru ... {}'.format(exc))
 
 def get_newsru():
-    html_newsru = urlopen("https://www.newsru.com/allnews/")
-    bs_obj = bs(html_newsru, 'lxml')
-    text_news_newsru = bs_obj.find('body').find_all('a', class_ = 'index-news-title')
-    time_news_newsru = bs_obj.find('body').find_all('span', class_ = 'index-news-date')
-    print('=== NEWSRU.COM - NEWS ===', end = '')
-    get_time()
+    try:
+        html_newsru = urlopen("https://www.newsru.com/allnews/")
+        bs_obj = bs(html_newsru, 'lxml')
+        text_news_newsru = bs_obj.find('body').find_all('a', class_ = 'index-news-title')
+        time_news_newsru = bs_obj.find('body').find_all('span', class_ = 'index-news-date')
+        print('=== NEWSRU.COM - NEWS ===', end = '')
+        get_time()
 
-    time_list_newsru = []
-    news_list_newsru = []
+        time_list_newsru = []
+        news_list_newsru = []
 
-    for j in time_news_newsru:
-        time_list_newsru.append(j.text[13:-47])
+        for j in time_news_newsru:
+            time_list_newsru.append(j.text[13:-47])
 
-    for i in text_news_newsru:
-        news_list_newsru.append(i.text[6:-5])
+        for i in text_news_newsru:
+            news_list_newsru.append(i.text[6:-5])
 
-    for k in range(len(time_list_newsru)):
-        all_string_news = "".join(time_list_newsru[k].split()) + ' ' + news_list_newsru[k]
-        print(all_string_news)
+        for k in range(len(time_list_newsru)):
+            all_string_news = "".join(time_list_newsru[k].split()) + ' ' + news_list_newsru[k]
+            print(all_string_news)
+    except Exception as exc:
+        print('Some problem ... newsru.com ... {}'.format(exc))
 
 get_news_echo_msk()
 get_newsru()
