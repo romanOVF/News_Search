1cdbcfbbcde4b0894d574668d1e10dcb2f02f193
romanOVF <rkovriginpost@google.com>
2019-09-03 15:59:43 +0300

    Added the ability to collect news simultaneously with ECHO MOSCOW and NEWSRU.COM

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # News_Search
 
 # 2019-09-03 This scraper was created to quickly receive news.
+# Added the ability to collect news simultaneously with ECHO MOSCOW and NEWSRU.COM
--- script.py
+++ script.py
@@ -3,12 +3,47 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
-import re
+import re, time
 
-html = urlopen("https://echo.msk.ru/")
-bs_obj = bs(html, 'lxml')
+def get_time():
+    date_time = time.ctime(time.time())
+    time_string = '{} {}'.format(' ', date_time)
+    print(time_string)
 
-texts = bs_obj.find('ul', class_ = 'newslist').find_all('h3')
+def get_news_echo_msk():
+    
+    html = urlopen("https://echo.msk.ru/")
+    bs_obj = bs(html, 'lxml')
+    time_news = bs_obj.find('div', class_ = 'newspreview').find_all('span', class_ = 'datetime')
+    text_news = bs_obj.find('ul', class_ = 'newslist').find_all('h3')
+    print('=== ECHO MOSCOW - NEWS ===', end = '')
+    get_time()
 
-for i in texts:
-    print(i.text, end = '')
+    for i in text_news:
+        print(" ".join(i.text.split()))
+
+    print()
+
+def get_newsru():
+    html = urlopen("https://www.newsru.com/allnews/")
+    bs_obj = bs(html, 'lxml')
+    texts = bs_obj.find('body').find_all('a', class_ = 'index-news-title')
+    time_news = bs_obj.find('body').find_all('span', class_ = 'index-news-date')
+    print('=== NEWSRU.COM - NEWS ===', end = '')
+    get_time()
+
+    time_list = []
+    news_list = []
+
+    for j in time_news:
+        time_list.append(j.text[13:-47])
+
+    for i in texts:
+        news_list.append(i.text[6:-5])
+
+    for k in range(len(time_list)):
+        all_string_news = "".join(time_list[k].split()) + ' ' + news_list[k]
+        print(all_string_news)
+
+get_news_echo_msk()
+get_newsru()
